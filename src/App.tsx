4471cff78ec8eba1cca71b06e0d53975152b0889
David Ortet <davidortetsabater@gmail.com>
2024-12-16 10:49:54 +0100

    Creating Complex Action

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import Counter from "./state-management/Counter";
+import LoginStatus from "./state-management/LoginStatus";
+import TaskList from "./state-management/TaskList";
 
 function App() {
-  return <Counter />;
+  return <LoginStatus />;
 }
 
 export default App;
