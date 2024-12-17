d1e89b6b63447fc78387adb96dff50206940eed1
David Ortet <davidortetsabater@gmail.com>
2024-12-17 09:24:37 +0100

    Inspecting Store with Zustand DevTools

--- src/state-management/NavBar.tsx
+++ src/state-management/NavBar.tsx
@@ -1,11 +1,12 @@
-import { useContext } from "react";
-import TasksContext from "./tasks/tasksContext";
 import LoginStatus from "./auth/LoginStatus";
 import useCounterStore from "./counter/store";
 
 const NavBar = () => {
   // const { tasks } = useContext(TasksContext);
   const { counter } = useCounterStore();
+  //* Evita que se renderize si el counter no cambia
+  //* const counter = useCounterStore((s) => s.counter);
+
   return (
     <nav className="navbar d-flex justify-content-between">
       <span className="badge text-bg-secondary">{counter}</span>
--- src/state-management/counter/store.ts
+++ src/state-management/counter/store.ts
@@ -1,15 +1,21 @@
 import { create } from "zustand";
+import { mountStoreDevtool } from "simple-zustand-devtools";
 
 interface CounterStore {
   counter: number;
+  max: number;
   increment: () => void;
   reset: () => void;
 }
 
 const useCounterStore = create<CounterStore>((set) => ({
   counter: 0,
+  max: 10,
   increment: () => set((state) => ({ counter: state.counter + 1 })),
   reset: () => set(() => ({ counter: 0 })),
 }));
+
+if (process.env.NODE_ENV === "development")
+  mountStoreDevtool("Counter Store", useCounterStore);
 
 export default useCounterStore;
